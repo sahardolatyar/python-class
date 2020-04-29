f9d59f14df9a94287435974d2e6637e8a06ca8b7
sahardolatyar <63776848+sahardolatyar@users.noreply.github.com>
2020-04-29 17:45:46 +0430

    Add files via upload

---
+++ ex-12.py
@@ -0,0 +1,12 @@
+import tkinter
+win=tkinter.Tk()
+lb1=tkinter.Label(win,text='user')
+lb1.pack()
+en1=tkinter.Entry(win)
+en1.pack()
+lb2=tkinter.Label(win,text='password')
+lb2.pack()
+en2=tkinter.Entry(win)
+en2.pack()
+btn=tkinter.Button(win,text='Enter')
+btn.pack()
