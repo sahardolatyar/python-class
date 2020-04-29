2a9078f1e66dd6a34fff28870cb5fb62990a34c5
sahardolatyar <63776848+sahardolatyar@users.noreply.github.com>
2020-04-29 11:30:19 +0430

    Add files via upload

---
+++ EX-7.py
@@ -0,0 +1,78 @@
+#tameine (012)
+a = int (input ("adade aval vared shavad"))
+b = int (input ("adade dovom vared shavad"))
+if a > b:
+    print (a,b)
+    
+else:
+    print (b,a)
+
+
+#tamrine(013)
+h = int (input ("yek adad kamtar az 20 vared konid"))
+if h > 20 or c==20 :
+    print ('too high')
+else:
+    print ('thank you')
+
+
+    
+#tamrine(014)
+f = int (input ('yek adad beyne 10 ta 20 vared shavad:'))
+if a<=20 and a>=10:
+    print('thank you')
+else:
+    print('incorrect answer')
+
+    
+#tamrine(015)
+    
+str1 = input ("range morede alagheye khod ra vared konid: ")
+if str1 == "red" or str1=="Red" or str1=="RED":
+    print ("I like red too")
+else:
+    print ("I don't like " + str1 + " , I prefer red")
+
+#tamrine(016)
+str2= str (input ('Is it raining? '))
+if str2 == "yes":
+    str3= str (input ('Is it windy? '))
+    if str3== 'yes':
+        print ('It is too windy for an umbrella')
+    else:
+        print ('Take an umbrella')
+else:
+    print ('Enjoy your day')
+
+
+#tamrine(017)
+n = int (input ("sene khod ra vared konid: "))
+if n > 18 or n == 18:
+    print ('you can vote')
+elif n == 17:
+    print ('you can learn to drive')
+elif n == 16:
+    print ('you can buy a lottery ticket')
+else:
+    print ('you can go Trick-or- Treating')
+
+#tamrine(018)
+i = int (input ( 'yek adad vared konid: '))
+if i < 10:
+    print ('Too low')
+if i > 9 and i <20:
+    print ('correct')
+else:
+    print ('Too High')
+
+#tamrine(019)
+k = int(input('yek adad vared konid:'))
+if k==1:
+    print('Thank you')
+elif k==2:
+    print('well done')
+elif k==3:
+    print('correct')
+else:
+    print('Error message')   
+    
